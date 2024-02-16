35fa5474433fc12bd4511670b87e3556e286cec1
Hellen Cristina <76890061+HellenCristinaP@users.noreply.github.com>
2024-02-16 19:20:12 -0300

    Update portifolio.js

---
+++ portifolio.js
@@ -0,0 +1,4 @@
+if (!localStorage.getItem('alertMostrado')) {
+    alert("Bem-vindo ao meu portifólio! Aqui você pode encontrar informações sobre mim e meus projetos.");
+    localStorage.setItem('alertMostrado', 'true');
+}
